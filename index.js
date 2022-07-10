c0a97abb9b0bd952deebb81a357492d748910399
hcPark <zipcodebaby@gmail.com>
2022-07-10 14:28:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//xpBWZNIcIU6tT1de mongodb
+
+const express = require('express')
+const app = express()
+const port = 7040
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hcpark:xpBWZNIcIU6tT1de@boilerplate.bvlwj.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log('MongoDB connected...')).catch(err => console.log(err))
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 아ㄴ녕ㅏ세')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
